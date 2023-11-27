769201916f3629054a74d7678eb6abbd37781ac8
CEAarab <c.e.aarab@gmail.com>
2023-11-27 04:48:28 -0500

    Quest 5

--- TP1.c
+++ TP1.c
@@ -5,9 +5,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define MAX_COMMAND_LENGTH 100
-#define PROMPT_2 "enseash [%s:%d] %%  "
+#define PROMPT_2 "enseash [%s:%d|%ldms] %%  "
 #define WELCOME_MESSAGE "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
 
 // A function used to write the very first welcome message using the write() function
@@ -16,7 +17,7 @@ void displayWelcomeMessage() {
 }
 
 // A function used to write the name of the prompt 'in our case enseash %' + the status depends on the first output.
-void displayPrompt(int status, bool isSignal) {
+void displayPrompt(int status, bool isSignal, long executionTime) {
     char prompt[MAX_COMMAND_LENGTH];
     char statusStr[10];
     if (isSignal) {
@@ -24,10 +25,17 @@ void displayPrompt(int status, bool isSignal) {
     } else {
         snprintf(statusStr, sizeof(statusStr), "exit");
     }
-    snprintf(prompt, sizeof(prompt), PROMPT_2, statusStr, status);
+    snprintf(prompt, sizeof(prompt), PROMPT_2, statusStr, status,executionTime);
     write(STDOUT_FILENO, prompt, strlen(prompt));
 }
+
+// Custom FUnction to calculate the exact time passed.
+long Time(struct timespec start, struct timespec end) {
+    return ((end.tv_sec - start.tv_sec) * 1000) + ((end.tv_nsec - start.tv_nsec) / 1000000);
+}
+
 int main() {
+    long executionTime = 0; // the first display will have 0ms
     char command[MAX_COMMAND_LENGTH];
     int lastStatus = 0;
     bool isSignal = false;
@@ -36,14 +44,15 @@ int main() {
 
 	
     while (1) { // We stay in the loop until the user run 'exit'
-        displayPrompt(lastStatus, isSignal);
+	displayPrompt(lastStatus, isSignal, executionTime);
 
+	
         if (read(STDIN_FILENO, command, sizeof(command)) <= 0) {
             write(STDOUT_FILENO, "\n", 1);
             break;
         }
 
-        // Delete the \n at the end of the read, which we added in line 29, since it might cause problems while executing the command
+        // Delete the \n at the end of the read, which we added, since it might cause problems while executing the command
         command[strcspn(command, "\n")] = '\0';
 	
 	// Check if the user want to exit
@@ -56,6 +65,11 @@ int main() {
 	if (strcmp(command, "") == 0) {
             strcpy(command, "date");
         }
+        
+        struct timespec start, end;
+        
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        
         // Create a child process to run the commands
         pid_t pid = fork();
 
@@ -76,6 +90,9 @@ int main() {
             // We wait until the end of the execution in the child process
             int status;
             waitpid(pid, &status, 0);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            
+            executionTime = Time(start, end);
 	    // Grab the status Code which depends on em we will set the lastStatus Variable used in displayPrompt()
             if (WIFEXITED(status)) {
                 lastStatus = WEXITSTATUS(status);
@@ -84,6 +101,9 @@ int main() {
                 lastStatus = WTERMSIG(status);
                 isSignal = true;
             }
+            
+            
+           
         }
     }
 
